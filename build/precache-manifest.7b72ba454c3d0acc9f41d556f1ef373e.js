d93197fa0ed4091fbf2f27b4928571b5aee44e0f
Shable <shableharris92@gmail.com>
2019-12-09 02:28:57 -0500

    updates

---
+++ build/precache-manifest.7b72ba454c3d0acc9f41d556f1ef373e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499786fd84765fa749b9440b808771c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ad15ee7502a71e5fb4",
+    "url": "/static/css/main.7d142976.chunk.css"
+  },
+  {
+    "revision": "1226ba3871e5be9557eb",
+    "url": "/static/js/2.2f6154bc.chunk.js"
+  },
+  {
+    "revision": "e1ad15ee7502a71e5fb4",
+    "url": "/static/js/main.af83d752.chunk.js"
+  },
+  {
+    "revision": "45dd92cc5ab7eb9d6675",
+    "url": "/static/js/runtime-main.2c5b7a34.js"
+  }
+]);
